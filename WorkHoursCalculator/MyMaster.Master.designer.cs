5e7b526a0703ddf98916821b7b8ab87052a0c829
Adel <adel.othman@live.com>
2017-07-18 17:19:58 +0200

    Invalid postback or callback argument FIXED

--- WorkHoursCalculator/MyMaster.Master.designer.cs
+++ WorkHoursCalculator/MyMaster.Master.designer.cs
@@ -49,6 +49,15 @@ namespace WorkHoursCalculator {
         protected global::System.Web.UI.WebControls.Button btnLogin;
         
         /// <summary>
+        /// btnRegister control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegister;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
