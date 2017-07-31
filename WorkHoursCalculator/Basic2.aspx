916bf0e6ace402f879f661d5c2574d525191cacd
shiteater <kojetajpokemon@yahoo.co.uk>
2017-07-31 21:12:19 +0200

    novo al ne radi event na save

--- WorkHoursCalculator/Basic2.aspx
+++ WorkHoursCalculator/Basic2.aspx
@@ -2,7 +2,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:TextBox ID="TbxWorkedHouresOnThisDay" runat="server" Width="180px">Worked Houres On This Day</asp:TextBox>
+     <asp:TextBox ID="TbxWorkedHouresOnThisDay" runat="server" Width="180px">Worked Houres On This Day</asp:TextBox>
     <asp:Calendar ID="Calendar1" runat="server" style="top: 23px; left: 1100px; float: right; height: 188px; width: 513px;" SelectedDate="<%# DateTime.Today %>" OnSelectionChanged="Calendar1_SelectionChanged" >
         <SelectedDayStyle BackColor="#6699FF" />
         <TodayDayStyle BackColor="#FFFFCC" />
@@ -21,10 +21,12 @@
     </asp:DropDownList>
     <br />
     <br />
+    Total Hours:
     <asp:TextBox ID="TbxTotalHours" runat="server" Width="180px">Total Hours</asp:TextBox>
     &nbsp;<br />
     <br />
     <br />
+    Total Earnings
     <asp:TextBox ID="TbxTotalEarnings" runat="server" Width="180px">Total Earnings</asp:TextBox>
     <br />
     <br />
--- WorkHoursCalculator/Basic2.aspx.cs
+++ WorkHoursCalculator/Basic2.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,7 +16,6 @@ namespace WorkHoursCalculator
         {
 
         }
-
         protected void BtnSaveChanges_Click(object sender, EventArgs e)
         {
             //promijenite con
@@ -25,7 +25,8 @@ namespace WorkHoursCalculator
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
             cmd.Parameters.AddWithValue("@V1", Convert.ToDateTime(Calendar1.SelectedDate));
-                                                    //trenutno ovo '1' je id korisnika koji trebamo povezati sa loginom
+
+            //trenutno ovo '1' je id korisnika koji trebamo povezati sa loginom
             cmd.CommandText = $"insert into dbo.Kalkulacije values( '1' , @V1, Null, Null,'{TbxWorkedHouresOnThisDay.Text}', '{TbxHourPrice.Text}')";
             cmd.ExecuteNonQuery();
             con.Close();
@@ -33,35 +34,64 @@ namespace WorkHoursCalculator
             con.Close();
         }
 
+
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
+            //promijenite con
+
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            string datum = Calendar1.SelectedDate.ToShortDateString();
 
-            // vađenje podataka iz sql i upisivanje u textboxeve
-
-            
-            string selectSql = "select * from Kalkulacije where Ukupno_odradeno_sati";
-            SqlCommand cmd = new SqlCommand(selectSql, con);
+            // možda napraviti try catch
 
             try
             {
-                con.Open();
+                // prilagoditi d.m.yyy. ili dd.mm.yyyy.
+                string converted = DateTime.ParseExact(datum, "d.M.yyyy.", CultureInfo.InvariantCulture)
+                              .ToString("yyyy-MM-dd");
+
+
+                cmd.Parameters.AddWithValue("@V1", converted);
+                cmd.CommandText = "select * from Kalkulacije where Datum LIKE @V1";
 
                 using (SqlDataReader read = cmd.ExecuteReader())
                 {
                     while (read.Read())
                     {
+                        // čitanje iz baze i upisivanje u tbx TbxTotalHours
                         TbxTotalHours.Text = (read["Ukupno_odradeno_sati"].ToString());
+                        var TotalHours = 0;
+                        Int32.TryParse(TbxTotalHours.Text, out TotalHours);
+
+                        // čitanje iz baze
+                        var TotalEarnings = (read["Satnica"].ToString());
+                        var _TotalEarnings = 0;
+                        Int32.TryParse(TotalEarnings, out _TotalEarnings);
+
+                        // upisivanje u TbxTotalEarnings
+                        var Ukupno = TotalHours * _TotalEarnings;
+                        TbxTotalEarnings.Text = Ukupno.ToString();
+
+                        //{
+                        //    // you know that the parsing attempt
+                        //    // was successful
+                        //}
+
 
                     }
                 }
             }
             finally
             {
-                con.Close();
+                // da mi ne zablokira app na nepotpunjenim datumima tako da mogu spremiti današnji dan
+                //radi sa praznim finally
             }
-        }
 
-        //upisivanje nema problema
+            con.Close();
+
+        }
     }
 }
