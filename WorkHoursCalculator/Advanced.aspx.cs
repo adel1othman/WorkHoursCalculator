3ddbfc7b2f35e194455211851efd710a7159130e
Adel <adel.othman@live.com>
2017-08-09 16:30:05 +0200

    Commit

--- WorkHoursCalculator/Advanced.aspx.cs
+++ WorkHoursCalculator/Advanced.aspx.cs
@@ -34,10 +34,10 @@ namespace WorkHoursCalculator
         protected void Calendar1_SelectionChanged(object sender, EventArgs e)
         {
             // Goran
-            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
+            //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
 
             // Adel
-            //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
 
             SqlCommand cmd = new SqlCommand("select * from Kalkulacije where Id_korisnik = @id and Datum like @myDatum", con);
             cmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
@@ -49,6 +49,7 @@ namespace WorkHoursCalculator
             {
                 var fromh = read["Pocetak_rv"];
                 var toh = read["Kraj_rv"];
+                var ukupno = read["Ukupno_odradeno_sati"];
                 var hr = read["Satnica"];
 
                 if (fromh.GetType() == typeof(TimeSpan) && toh.GetType() == typeof(TimeSpan))
@@ -61,8 +62,17 @@ namespace WorkHoursCalculator
                 {
                     TextBox1.Text = String.Empty;
                     TextBox2.Text = String.Empty;
-                    TextBox3.Text = String.Empty;
+                    TextBox3.Text = hr.ToString();
                 }
+
+                lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
+                lblUkupnoR.Text = ukupno.ToString();
+                lblFee.Text = "You've earned:";
+                lblFeeR.Text = ((int)hr * (int)ukupno).ToString();
+                lblUkupno.Visible = true;
+                lblUkupnoR.Visible = true;
+                lblFee.Visible = true;
+                lblFeeR.Visible = true;
             }
             else
             {
@@ -96,10 +106,10 @@ namespace WorkHoursCalculator
             }
 
             // Goran
-            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
+            //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
 
             // Adel
-            //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
 
             SqlCommand cmd = new SqlCommand("select * from Kalkulacije where Id_korisnik = @id and Datum like @myDatum", con);
             cmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
@@ -117,82 +127,88 @@ namespace WorkHoursCalculator
         {
             int fromh = 0, toh = 0, hr = 0;
 
-            if (int.TryParse(TextBox1.Text, out fromh) && int.TryParse(TextBox2.Text, out toh) && int.TryParse(TextBox3.Text, out hr) && Calendar1.SelectedDate != null)
+            if (int.TryParse(TextBox1.Text, out fromh) && int.TryParse(TextBox2.Text, out toh) && int.TryParse(TextBox3.Text, out hr))
             {
-                //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
-
-                // Adel
-                SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
-
-                SqlCommand cmd = new SqlCommand("select * from Kalkulacije where Id_korisnik = @id and Datum like @myDatum", con);
-                cmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
-                cmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
-
-                con.Open();
-                SqlDataReader read = cmd.ExecuteReader();
-                if (read.Read())
+                int ukupno = 0;
+                if (toh > fromh)
                 {
-                    con.Close();
+                    ukupno = toh - fromh;
+                }
+                else if (toh < fromh)
+                {
+                    ukupno = toh + (24 - fromh);
+                }
+                if (ddlSaveCalculation.SelectedIndex == 0)
+                {
+                    // Goran
+                    //SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
 
-                    SqlCommand updateCmd = new SqlCommand("update Kalkulacije set Pocetak_rv = @prv, Kraj_rv = @krv, Ukupno_odradeno_sati = @ukupno, Satnica = @hr where Id_korisnik = @id and Datum like @myDatum", con);
-                    updateCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
-                    updateCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
-                    updateCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
-                    updateCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
-                    updateCmd.Parameters.AddWithValue("@hr", hr);
-                    int ukupno = 0;
-                    if (toh > fromh)
-                    {
-                        ukupno = toh - fromh;
-                    }
-                    else if (toh < fromh)
-                    {
-                        ukupno = toh + (24 - fromh);
-                    }
-                    updateCmd.Parameters.AddWithValue("@ukupno", ukupno);
+                    // Adel
+                    SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-FR7RPIJ\SQLEXPRESS;Initial Catalog=WorkHourCalculator;Integrated Security=True;Pooling=False");
+
+                    SqlCommand cmd = new SqlCommand("select * from Kalkulacije where Id_korisnik = @id and Datum like @myDatum", con);
+                    cmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                    cmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
 
                     con.Open();
+                    SqlDataReader read = cmd.ExecuteReader();
+                    if (read.Read())
+                    {
+                        con.Close();
 
-                    int rowsAffected = updateCmd.ExecuteNonQuery();
+                        SqlCommand updateCmd = new SqlCommand("update Kalkulacije set Pocetak_rv = @prv, Kraj_rv = @krv, Ukupno_odradeno_sati = @ukupno, Satnica = @hr where Id_korisnik = @id and Datum like @myDatum", con);
+                        updateCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                        updateCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
+                        updateCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
+                        updateCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
+                        updateCmd.Parameters.AddWithValue("@hr", hr);
+                        updateCmd.Parameters.AddWithValue("@ukupno", ukupno);
 
-                    con.Close();
+                        con.Open();
 
-                    lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
-                    lblUkupnoR.Text = ukupno.ToString();
-                    lblFee.Text = "You've earned:";
-                    lblFeeR.Text = (hr * ukupno).ToString();
-                    lblUkupno.Visible = true;
-                    lblUkupnoR.Visible = true;
-                    lblFee.Visible = true;
-                    lblFeeR.Visible = true;
+                        int rowsAffected = updateCmd.ExecuteNonQuery();
+
+                        con.Close();
+
+                        lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
+                        lblUkupnoR.Text = ukupno.ToString();
+                        lblFee.Text = "You've earned:";
+                        lblFeeR.Text = (hr * ukupno).ToString();
+                        lblUkupno.Visible = true;
+                        lblUkupnoR.Visible = true;
+                        lblFee.Visible = true;
+                        lblFeeR.Visible = true;
+                    }
+                    else
+                    {
+                        con.Close();
+
+                        SqlCommand insertCmd = new SqlCommand("insert into Kalkulacije values(@id, @myDatum, @prv, @krv, @ukupno, @hr)", con);
+                        insertCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                        insertCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
+                        insertCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
+                        insertCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
+                        insertCmd.Parameters.AddWithValue("@hr", hr);
+                        insertCmd.Parameters.AddWithValue("@ukupno", ukupno);
+
+                        con.Open();
+
+                        int rowsAffected = insertCmd.ExecuteNonQuery();
+
+                        con.Close();
+
+                        lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
+                        lblUkupnoR.Text = ukupno.ToString();
+                        lblFee.Text = "You've earned:";
+                        lblFeeR.Text = (hr * ukupno).ToString();
+                        lblUkupno.Visible = true;
+                        lblUkupnoR.Visible = true;
+                        lblFee.Visible = true;
+                        lblFeeR.Visible = true;
+                    }
                 }
                 else
                 {
-                    con.Close();
-
-                    SqlCommand insertCmd = new SqlCommand("insert into Kalkulacije values(@id, @myDatum, @prv, @krv, @ukupno, @hr)", con);
-                    insertCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
-                    insertCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
-                    insertCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
-                    insertCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
-                    insertCmd.Parameters.AddWithValue("@hr", hr);
-                    int ukupno = 0;
-                    if (toh > fromh)
-                    {
-                        ukupno = toh - fromh;
-                    }
-                    else if (toh < fromh)
-                    {
-                        ukupno = toh + (24 - fromh);
-                    }
-                    insertCmd.Parameters.AddWithValue("@ukupno", ukupno);
-                    
-                    con.Open();
-
-                    int rowsAffected = insertCmd.ExecuteNonQuery();
-
-                    con.Close();
-
                     lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
                     lblUkupnoR.Text = ukupno.ToString();
                     lblFee.Text = "You've earned:";
--- WorkHoursCalculator/Advanced.aspx.designer.cs
+++ WorkHoursCalculator/Advanced.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace WorkHoursCalculator {
         protected global::System.Web.UI.WebControls.DropDownList ddlSelection;
         
         /// <summary>
+        /// ddlSaveCalculation control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSaveCalculation;
+        
+        /// <summary>
         /// Calendar1 control.
         /// </summary>
         /// <remarks>
