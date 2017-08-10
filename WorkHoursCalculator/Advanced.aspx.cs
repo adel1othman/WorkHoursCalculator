2b03f66ac853aa48b484e985de87239b54d05722
Adel <adel.othman@live.com>
2017-08-10 16:09:20 +0200

    Kalkulacije za mj. i god.

--- WorkHoursCalculator/Advanced.aspx.cs
+++ WorkHoursCalculator/Advanced.aspx.cs
@@ -45,7 +45,7 @@ namespace WorkHoursCalculator
 
             con.Open();
             SqlDataReader read = cmd.ExecuteReader();
-            if (read.Read())
+            if (ddlSelection.SelectedIndex == 0 && read.Read())
             {
                 var fromh = read["Pocetak_rv"];
                 var toh = read["Kraj_rv"];
@@ -74,6 +74,26 @@ namespace WorkHoursCalculator
                 lblFee.Visible = true;
                 lblFeeR.Visible = true;
             }
+            else if (read.Read())
+            {
+                var fromh = read["Pocetak_rv"];
+                var toh = read["Kraj_rv"];
+                var ukupno = read["Ukupno_odradeno_sati"];
+                var hr = read["Satnica"];
+
+                if (fromh.GetType() == typeof(TimeSpan) && toh.GetType() == typeof(TimeSpan))
+                {
+                    TextBox1.Text = ((TimeSpan)fromh).TotalHours.ToString();
+                    TextBox2.Text = ((TimeSpan)toh).TotalHours.ToString();
+                    TextBox3.Text = hr.ToString();
+                }
+                else
+                {
+                    TextBox1.Text = String.Empty;
+                    TextBox2.Text = String.Empty;
+                    TextBox3.Text = hr.ToString();
+                }
+            }
             else
             {
                 TextBox1.Text = String.Empty;
@@ -81,6 +101,71 @@ namespace WorkHoursCalculator
                 TextBox3.Text = String.Empty;
             }
             con.Close();
+
+            if (ddlSelection.SelectedIndex == 1)
+            {
+                SqlCommand cmd1 = new SqlCommand("select sum(Ukupno_odradeno_sati) from Kalkulacije where Id_korisnik = @id and month(Datum)=@myMonth and year(Datum)=@myYear", con);
+                cmd1.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                cmd1.Parameters.AddWithValue("@myMonth", Calendar1.SelectedDate.Month);
+                cmd1.Parameters.AddWithValue("@myYear", Calendar1.SelectedDate.Year);
+
+                int odradeno = 0;
+
+                con.Open();
+                odradeno = (int)cmd1.ExecuteScalar();
+                con.Close();
+
+                SqlCommand cmd2 = new SqlCommand("select sum(zaradeno) from Kalkulacije where Id_korisnik = @id and month(Datum)=@myMonth and year(Datum)=@myYear", con);
+                cmd2.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                cmd2.Parameters.AddWithValue("@myMonth", Calendar1.SelectedDate.Month);
+                cmd2.Parameters.AddWithValue("@myYear", Calendar1.SelectedDate.Year);
+
+                int result = 0;
+
+                con.Open();
+                result = (int)cmd2.ExecuteScalar();
+                con.Close();
+
+                lblUkupno.Text = "Worked hours for this month: ";
+                lblUkupnoR.Text = odradeno.ToString();
+                lblFee.Text = "You've earned: ";
+                lblFeeR.Text = result.ToString();
+                lblUkupno.Visible = true;
+                lblUkupnoR.Visible = true;
+                lblFee.Visible = true;
+                lblFeeR.Visible = true;
+            }
+            else if (ddlSelection.SelectedIndex == 2)
+            {
+                SqlCommand cmd3 = new SqlCommand("select sum(Ukupno_odradeno_sati) from Kalkulacije where Id_korisnik = @id and year(datum)=@myYear", con);
+                cmd3.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                cmd3.Parameters.AddWithValue("@myYear", Calendar1.SelectedDate.Year);
+
+                int odradeno = 0;
+
+                con.Open();
+                odradeno = (int)cmd3.ExecuteScalar();
+                con.Close();
+
+                SqlCommand cmd4 = new SqlCommand("select sum(zaradeno) from Kalkulacije where Id_korisnik = @id and year(datum)=@myYear", con);
+                cmd4.Parameters.AddWithValue("@id", (int)Session["idKor"]);
+                cmd4.Parameters.AddWithValue("@myYear", Calendar1.SelectedDate.Year);
+
+                int result = 0;
+
+                con.Open();
+                result = (int)cmd4.ExecuteScalar();
+                con.Close();
+
+                lblUkupno.Text = "Worked hours for this year: ";
+                lblUkupnoR.Text = odradeno.ToString();
+                lblFee.Text = "You've earned: ";
+                lblFeeR.Text = result.ToString();
+                lblUkupno.Visible = true;
+                lblUkupnoR.Visible = true;
+                lblFee.Visible = true;
+                lblFeeR.Visible = true;
+            }
         }
 
         protected void btnAdd_Click(object sender, EventArgs e)
@@ -156,13 +241,14 @@ namespace WorkHoursCalculator
                     {
                         con.Close();
 
-                        SqlCommand updateCmd = new SqlCommand("update Kalkulacije set Pocetak_rv = @prv, Kraj_rv = @krv, Ukupno_odradeno_sati = @ukupno, Satnica = @hr where Id_korisnik = @id and Datum like @myDatum", con);
+                        SqlCommand updateCmd = new SqlCommand("update Kalkulacije set Pocetak_rv = @prv, Kraj_rv = @krv, Ukupno_odradeno_sati = @ukupno, Satnica = @hr, zaradeno = @earned where Id_korisnik = @id and Datum like @myDatum", con);
                         updateCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
                         updateCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
                         updateCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
                         updateCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
                         updateCmd.Parameters.AddWithValue("@hr", hr);
                         updateCmd.Parameters.AddWithValue("@ukupno", ukupno);
+                        updateCmd.Parameters.AddWithValue("@earned", ukupno * hr);
 
                         con.Open();
 
@@ -183,13 +269,14 @@ namespace WorkHoursCalculator
                     {
                         con.Close();
 
-                        SqlCommand insertCmd = new SqlCommand("insert into Kalkulacije values(@id, @myDatum, @prv, @krv, @ukupno, @hr)", con);
+                        SqlCommand insertCmd = new SqlCommand("insert into Kalkulacije values(@id, @myDatum, @prv, @krv, @ukupno, @hr, @earned)", con);
                         insertCmd.Parameters.AddWithValue("@id", (int)Session["idKor"]);
                         insertCmd.Parameters.AddWithValue("@myDatum", Calendar1.SelectedDate.ToString("yyyy-MM-dd"));
                         insertCmd.Parameters.AddWithValue("@prv", TimeSpan.FromHours(fromh));
                         insertCmd.Parameters.AddWithValue("@krv", TimeSpan.FromHours(toh));
                         insertCmd.Parameters.AddWithValue("@hr", hr);
                         insertCmd.Parameters.AddWithValue("@ukupno", ukupno);
+                        insertCmd.Parameters.AddWithValue("@earned", ukupno * hr);
 
                         con.Open();
 
@@ -209,15 +296,20 @@ namespace WorkHoursCalculator
                 }
                 else
                 {
-                    lblUkupno.Text = "Worked hours for date " + Calendar1.SelectedDate.ToShortDateString() + ":";
-                    lblUkupnoR.Text = ukupno.ToString();
-                    lblFee.Text = "You've earned:";
-                    lblFeeR.Text = (hr * ukupno).ToString();
-                    lblUkupno.Visible = true;
-                    lblUkupnoR.Visible = true;
-                    lblFee.Visible = true;
-                    lblFeeR.Visible = true;
+                    
                 }
+            }
+        }
+
+        protected void ddlSaveCalculation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlSaveCalculation.SelectedIndex == 0)
+            {
+                ddlSelection.Enabled = false;
+            }
+            else
+            {
+                ddlSelection.Enabled = true;
             }
         }
     }
